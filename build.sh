5032b4146b09a439444a411440e25a2c5263c3bd
zchen565 <37888282+zchen565@users.noreply.github.com>
2020-07-30 03:57:56 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-docker pull ubuntu:20.04
-
-#Generate the Dockerinfo file (with custom user)
-USERNAME=`id -u -n`
-GROUPNAME=`id -g -n`
-MY_UID=`id -u`
-MY_GID=`id -g`
-
-docker build -t ubuntu2004-dedos-ndlog --build-arg UNAME=${USERNAME} --build-arg GNAME=${GROUPNAME} --build-arg UID=${MY_UID} --build-arg GID=${MY_GID} .
