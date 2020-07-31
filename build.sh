a3cb2cf58dec6f880cdce18c18adf678c7add207
zchen565 <37888282+zchen565@users.noreply.github.com>
2020-07-30 22:05:21 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-docker pull ubuntu:16.04
-
-#Generate the Dockerinfo file (with custom user)
-USERNAME=`id -u -n`
-GROUPNAME=`id -g -n`
-MY_UID=`id -u`
-MY_GID=`id -g`
-
-docker build -t ubuntu-1604 --build-arg UNAME=${USERNAME} --build-arg GNAME=${GROUPNAME} --build-arg UID=${MY_UID} --build-arg GID=${MY_GID} .
